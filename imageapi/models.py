57dd1b6c0cbb1115d244982f575649129b95c395
eddym <eddyats11"gmail.com>
2021-09-08 23:23:46 +0300

    admin regs

--- imageapi/models.py
+++ imageapi/models.py
@@ -5,4 +5,4 @@ class Hero(models.Model):
     name = models.CharField(max_length=60)
     alias = models.CharField(max_length=60)
     def __str__(self):
-        return self.nameclass
+        return self.name
